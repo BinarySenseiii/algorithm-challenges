25a548c9c993f9484a6286acdee96e908eca29a8
Faisal Tariq <faisaltariq183@gmail.com>
2024-02-23 17:28:03 -0800

    done- count-occurances

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,38 @@
-function countOccurrences() {}
+// Solution-01
+function countOccurrences(str, char) {
+	str = String(str).toLowerCase()
+	char = String(char).toLowerCase()
 
-module.exports = countOccurrences;
+	const characterCountMap = {}
+
+	for (const character of str) {
+		if (!characterCountMap.hasOwnProperty(character)) {
+			characterCountMap[character] = 0
+		}
+
+		characterCountMap[character] = characterCountMap[character] + 1
+	}
+
+	const characterCount = characterCountMap[char]
+	return characterCount ?? 0
+}
+
+// Solution 02
+/* function countOccurrences(str, char) {
+	str = String(str).toLowerCase()
+	char = String(char).toLowerCase()
+
+	let count = 0
+	Array.from(str).forEach(character => character === char && count++)
+
+	return count
+} */
+
+// solution 03
+// const countOccurrences = (str, char) =>
+// 	Array.from(str).reduce(
+// 		(acc, character) => (character === char ? acc + 1 : acc),
+// 		0,
+// 	)
+
+module.exports = countOccurrences
